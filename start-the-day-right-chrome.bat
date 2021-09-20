597a7ed3c907161f86b7495d241d9fb0084be55b
Mark Cabie <markus.c000@gmail.com>
2021-09-20 15:53:08 +0800

    Added comment for feature 1

--- start-the-day-right-chrome.bat
+++ start-the-day-right-chrome.bat
@@ -1,9 +1,9 @@
 @echo off
 
 REM This is an app that opens relevant websites at the start of your day!
-
 start chrome "http://intranet.deltek.com" "http://resourcescheduler.deltek.com/resourcescheduler/default.asp#"
 
+REM Below are not really useful sites, but still added them just in case...
 start chrome "https://fb.com" "https://messenger.com" 
 
 exit
