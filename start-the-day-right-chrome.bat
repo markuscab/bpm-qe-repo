27fac7fca69a12040f3ac9564f52915a944b35b6
Mark Cabie <markus.c000@gmail.com>
2021-09-20 15:43:39 +0800

    Added comment for the .bat file

--- start-the-day-right-chrome.bat
+++ start-the-day-right-chrome.bat
@@ -1,3 +1,6 @@
 @echo off
+
+REM This is an app that opens relevant websites at the start of your day!
+
 start chrome "http://intranet.deltek.com" "http://resourcescheduler.deltek.com/resourcescheduler/default.asp#"
 exit
